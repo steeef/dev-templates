4b4473f7eea2488eac27c21ddd99eb69048e35e6
Stephen Price <stephen@stp5.net>
2023-03-23 19:49:11 -0700

    clean up setup

--- python38/flake.nix
+++ python38/flake.nix
@@ -4,39 +4,32 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-utils.url = "github:numtide/flake-utils";
-    mach-nix.url = "github:/DavHau/mach-nix";
   };
 
   outputs =
     { self
     , nixpkgs
     , flake-utils
-    , mach-nix
     }:
 
     flake-utils.lib.eachDefaultSystem (system:
     let
-      overlays = [
-        (self: super: {
-          machNix = mach-nix.defaultPackage.${system};
-          python = super.python38;
-        })
+      pkgs = import nixpkgs { inherit system; };
+      pythonPackages = pkgs.python38Packages;
+      venvDir = "./.venv";
+      packages = with pythonPackages; [
+        python
+        venvShellHook
       ];
-
-      pkgs = import nixpkgs { inherit overlays system; };
+      postShellHook = ''
+        PYTHONPATH=\$PWD/\${venvDir}/\${pythonPackages.python.sitePackages}/:\$PYTHONPATH
+      '';
     in
     {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ python machNix virtualenv ] ++
-          (with pkgs.python38Packages; [ pip ]);
-
-        shellHook = ''
-          ${pkgs.python}/bin/python --version
-          if [ ! -d "./.venv" ]; then
-            ${pkgs.python}/bin/python -m venv ./.venv
-          fi
-          source ./.venv/bin/activate
-        '';
+        inherit venvDir;
+        packages = packages;
+        postShellHook = postShellHook;
       };
     });
 }
