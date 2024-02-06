fee0b5606da8120518edbce0e62eaa632783e48b
Stephen Price <stephen@stp5.net>
2024-02-06 13:17:22 -0800

    updatin

--- python39/flake.nix
+++ python39/flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based Python 3.9 development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -17,6 +17,7 @@
       pkgs = import nixpkgs { inherit system; };
       pythonPackages = pkgs.python39Packages;
       venvDir = "./.venv";
+      buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.openssl ];
       packages = with pythonPackages; [
         python
         venvShellHook
@@ -28,7 +29,7 @@
     {
       devShells.default = pkgs.mkShell {
         inherit venvDir;
-        buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.openssl ];
+        buildInputs = buildInputs;
         packages = packages;
         postShellHook = postShellHook;
       };
