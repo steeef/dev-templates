46f554a0d1c004a41c0269fecbae6ef182a152b5
Stephen Price <stephen@stp5.net>
2024-04-25 13:08:13 -0700

    add node20

---
+++ node20/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [
+        (self: super: rec {
+          nodejs = super.nodejs_20;
+          pnpm = super.nodePackages.pnpm;
+          yarn = super.yarn.override {inherit nodejs;};
+        })
+      ];
+      pkgs = import nixpkgs {
+        inherit overlays system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [node2nix nodejs pnpm yarn];
+
+        shellHook = ''
+          echo "node `${pkgs.nodejs}/bin/node --version`"
+        '';
+      };
+    });
+}
