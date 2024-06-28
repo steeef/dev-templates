9e40f13fa6b72a8b3ad8d22bf0e3baf185d0088f
Stephen Price <stephen@stp5.net>
2024-06-28 09:24:24 -0700

    update pythons

--- python310/flake.nix
+++ python310/flake.nix
@@ -2,22 +2,20 @@
   description = "A Nix-flake-based Python 3.10 development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
       pythonPackages = pkgs.python310Packages;
       venvDir = "./.venv";
-      buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.openssl ];
+      buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.openssl];
       packages = with pythonPackages; [
         python
         venvShellHook
@@ -25,8 +23,7 @@
       postShellHook = ''
         PYTHONPATH="$(pwd)/${venvDir}/${pythonPackages.python.sitePackages}/:$PYTHONPATH"
       '';
-    in
-    {
+    in {
       devShells.default = pkgs.mkShell {
         inherit venvDir;
         buildInputs = buildInputs;
--- python311/flake.nix
+++ python311/flake.nix
@@ -2,22 +2,20 @@
   description = "A Nix-flake-based Python 3.11 development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
       pythonPackages = pkgs.python311Packages;
       venvDir = "./.venv";
-      buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.openssl ];
+      buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.openssl];
       packages = with pythonPackages; [
         python
         venvShellHook
@@ -26,8 +24,7 @@
         PYTHONPATH="$(pwd)/${venvDir}/${pythonPackages.python.sitePackages}/:$PYTHONPATH"
 
       '';
-    in
-    {
+    in {
       devShells.default = pkgs.mkShell {
         buildInputs = buildInputs;
         inherit venvDir;
